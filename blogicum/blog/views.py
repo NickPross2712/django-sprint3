f57a91e9465e22893252ba129d5e58d1ad2effc8
Nikita Prosvev <nickpross2712@gmail.com>
2025-05-03 10:16:29 +0300

    поправил flake8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,20 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Category, Post
-from .constants import POSTS_PER_PAGE, POST_ORDERING  # Импорт констант
+from .constants import POSTS_PER_PAGE, POST_ORDERING
 
 
 def get_published_posts():
-    """Возвращает QuerySet опубликованных постов с оптимизированными запросами."""
-    return Post.objects.select_related('category', 'author', 'location').filter(
-        pub_date__lte=timezone.now(),
-        is_published=True,
-        category__is_published=True
-    ).order_by(POST_ORDERING)  # Используем константу
+    return (
+        Post.objects
+        .select_related('category', 'author', 'location')
+        .filter(
+            pub_date__lte=timezone.now(),
+            is_published=True,
+            category__is_published=True
+        )
+        .order_by(POST_ORDERING)
+    )
 
 
 def index(request):
-    post_list = get_published_posts()[:POSTS_PER_PAGE]  # Используем константу
+    post_list = get_published_posts()[:POSTS_PER_PAGE]
     return render(request, 'blog/index.html', {'post_list': post_list})
 
 
